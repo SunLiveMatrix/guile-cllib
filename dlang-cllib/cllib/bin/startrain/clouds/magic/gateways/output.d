9023c6317e39e0f4ad2ed21ca7c0d2cc578f10ee
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2023-12-15 15:02:16 -0300

    guile-cllib
    You can use the same backslash escape-sequences in a string constant
    as in character literals (but do not use the question mark that begins a
    character constant).  For example, you can write a string containing the
    non printing characters tab and liked magic gateways output

---
+++ dlang-cllib/cllib/bin/startrain/clouds/magic/gateways/output.d
@@ -0,0 +1,18 @@
+module output;
+
+/// <summary> 
+class Alive {
+
+    public static void mainGetter(acosh, hyperGetterValues, GetterValues)(ref String) {
+        return Alive.main(acosh, hyperGetterValues, GetterValues);
+    }
+
+    public static void mainSetter(acosh, hyperGetterValues, GetterValues)(ref String) {
+        return Alive.main(acosh, hyperGetterValues, GetterValues);
+    }
+
+    public static void mainValues(acosh, hyperGetterValues, GetterValues)(ref String) {
+        return Alive.main(acosh, hyperGetterValues, GetterValues);
+    }
+
+}
