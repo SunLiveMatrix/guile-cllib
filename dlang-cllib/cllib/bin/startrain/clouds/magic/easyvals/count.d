914bb11a3f06c6c9c4340a73cb06b797a63be72a
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2023-12-15 14:20:21 -0300

    guile-cllib
    You can use the same backslash escape-sequences in a string constant
    as in character literals (but do not use the question mark that begins a
    character constant).  For example, you can write a string containing the
    non printing characters tab and liked loop easy vals

---
+++ dlang-cllib/cllib/bin/startrain/clouds/magic/easyvals/count.d
@@ -0,0 +1,52 @@
+module count;
+
+/// <summary: count of the number of items in the collection>
+class CountReverseGetters {
+
+    public static void IHeaderObject(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseGetters.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+    public static void IHeaderString(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseGetters.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+    public static void IHeaderEvent(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseGetters.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+}
+
+class CountReverseSettings {
+
+    public static void IHeaderObject(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseSettings.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+    public static void IHeaderString(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseSettings.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+    public static void IHeaderEvent(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseSettings.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+}
+
+class CountReverseValues {
+
+    public static void IHeaderObject(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseValues.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+    public static void IHeaderString(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseValues.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+    public static void IHeaderEvent(APIString, APIObject, APIReverseWill)(ref String) { // @suppress(dscanner.style.phobos_naming_convention)
+          return CountReverseValues.IHeaderString(APIString, APIObject, APIReverseWill);
+    }
+    
+}
+   
+   
